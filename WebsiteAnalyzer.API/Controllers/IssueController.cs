bdf14ad7d4132181c175eb1cb65b513740961231
Aryan09-dev <solankiaryan2801@gmail.com>
2026-02-05 21:17:10 +0530

    necessary controllers added and jwt paramter implement in the appsettings.json

---
+++ WebsiteAnalyzer.API/Controllers/IssueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteAnalyzer.API.Controllers
+{
+    public class IssueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebsiteAnalyzer.API/Controllers/PerformanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteAnalyzer.API.Controllers
+{
+    public class PerformanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
